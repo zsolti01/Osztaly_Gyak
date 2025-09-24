513eb1def6b264e746cef4b415ca7de3d8c9c51f
vecseizs <vecseizs@kkszki.hu>
2025-09-24 09:27:45 +0200

    Feladat 4 and 5 is ready

--- Osztaly_Gyak/Program.cs
+++ Osztaly_Gyak/Program.cs
@@ -24,7 +24,7 @@ namespace Osztaly_Gyak
         this.szelesseg = szelesseg;
     }
 
-    public int ArSzamolalo()
+    public int ArSzamlalo()
     {
         double terulet = (hossz / 100.0) * (szelesseg / 100.0);
         if (tipus == "tartólap")
@@ -35,10 +35,57 @@ namespace Osztaly_Gyak
             return 0;
     }
 
+    public class Butor
+    {
+        private Butorlap[] lapok;
+        private int lapokSzama;
+
+        public Butor()
+        {
+            lapok = new Butorlap[100];
+            lapokSzama = 0;
+        }
+
+        public void LapHozzaad(Butorlap lap)
+        {
+            if (lapokSzama < lapok.Length)
+            {
+                lapok[lapokSzama] = lap;
+                lapokSzama++;
+            }
+        }
+
+        public int ArSzamol()
+        {
+            int osszeg = 0;
+            for (int i = 0; i < lapokSzama; i++)
+            {
+                osszeg += lapok[i].ArSzamlalo();
+            }
+            return osszeg;
+        }
+
+        public override string ToString()
+        {
+            return $"Butor - lapok: {lapokSzama}, ar {ArSzamol()} Ft";
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            var butor = new butorgyar.Butor();
+
+            var lap1 = new butorgyar.Butorlap("tartólap", 200, 60);
+            var lap2 = new butorgyar.Butorlap("tartólap", 100, 60);
+            var lap3 = new butorgyar.Butorlap("hátlap", 200, 100);
+
+            butor.LapHozzaad(lap1);
+            butor.LapHozzaad(lap2);
+            butor.LapHozzaad(lap3);
+
+            Console.WriteLine(butor);
         }
     }
 }
