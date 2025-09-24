fbef45c6fcbabfc9a379c7d52aa5c041ef8b8f8c
vecseizs <vecseizs@kkszki.hu>
2025-09-24 08:00:39 +0200

    Feladat 1 is ready

--- Osztaly_Gyak/Program.cs
+++ Osztaly_Gyak/Program.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 
 namespace Osztaly_Gyak
 {
+    public class Butorlap
+    {
+        public int hosszusag;
+        public int szelesseg;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
