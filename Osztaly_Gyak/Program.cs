35a73a7e5f1ff59679e57f188f664fe32dad0446
vecseizs <vecseizs@kkszki.hu>
2025-09-24 09:21:07 +0200

    Feladat 2 is ready

--- Osztaly_Gyak/Program.cs
+++ Osztaly_Gyak/Program.cs
@@ -9,9 +9,20 @@ namespace Osztaly_Gyak
 {
     public class Butorlap
     {
-        public int hosszusag;
-        public int szelesseg;
+        private int hossz;
+        private int szelesseg;
+        private string tipus;
+
+        public const int tartolap = 5000;
+        public const int hatlap = 500;
     }
+    
+    public Butorlap(string tipus, int hossz, int szelesseg)
+        {
+            this.tipus = tipus;
+            this.hossz = hossz;
+            this.szelesseg = szelesseg;
+        }
 
     internal class Program
     {
